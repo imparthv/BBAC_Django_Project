3e6cb3cdbf8a3c004612f7e6edda032837a4a031
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-11-13 16:33:45 +0530

    Defined API routes

--- BBAC/BBAC/urls.py
+++ BBAC/BBAC/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from club.views import MemberViewSet, EventViewSet, ParticipationViewSet
+
+router = routers.DefaultRouter()
+router.register(r'members', MemberViewSet) 
+router.register(r'event', EventViewSet)
+router.register(r'participations', ParticipationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
