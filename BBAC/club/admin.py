f7a556c01457188ded95aafe28e7eb43d8ffce88
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-11-13 12:34:10 +0530

    Changed field name

--- BBAC/club/admin.py
+++ BBAC/club/admin.py
@@ -3,4 +3,9 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Member)
+# admin.site.register(Member)
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'role', 'joined_date')
+    search_fields = ('name', 'email')
+    list_filter = ('role',)
--- BBAC/club/models.py
+++ BBAC/club/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-
+# Represents members of club
 class Member(models.Model):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('volunteer', 'Volunteer'),
+        ('educator', 'Educator'),
+        ('admin', 'Admin')
+
+    ]
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    join_date = models.DateField(auto_now_add=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    joined_date = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
 
     def __str__(self):
-        return self.name
+        return f"{self.name}({self.role})"
