dcdbbaaaf2dc6f7017ea8cfd46ce3047b127e448
Parth Vasdewani <imparthvasdewani@gmail.com>
2025-11-13 16:32:18 +0530

    Defined API behavior using ModelViewSet

--- BBAC/club/views.py
+++ BBAC/club/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import *
+from .serializers import MemberSerializer, EventSerializer, ParticipationSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class ParticipationViewSet(viewsets.ModelViewSet):
+    queryset = Participation.objects.all()
+    serializer_class = ParticipationSerializer
+
+
